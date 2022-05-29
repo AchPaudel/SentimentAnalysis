6d5a53b3ead85eaa7c24e99bc2050d8e106faa84
achyut <a953p488@ku.edu>
2022-05-29 15:40:07 -0500

    created notebook for easier debugging

--- index.py
+++ index.py
@@ -3,55 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nlp
 import random
-import pandas as pd
-import csv
-
-
-def show_history(h):
-    epochs_trained = len(h.history['loss'])
-    plt.figure(figsize=(16, 6))
-
-    plt.subplot(1, 2, 1)
-    plt.plot(range(0, epochs_trained), h.history.get('accuracy'), label='Training')
-    plt.plot(range(0, epochs_trained), h.history.get('val_accuracy'), label='Validation')
-    plt.ylim([0., 1.])
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.legend()
-
-    plt.subplot(1, 2, 2)
-    plt.plot(range(0, epochs_trained), h.history.get('loss'), label='Training')
-    plt.plot(range(0, epochs_trained), h.history.get('val_loss'), label='Validation')
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
-    plt.legend()
-    plt.show()
-
-    
-def show_confusion_matrix(y_true, y_pred, classes):
-    from sklearn.metrics import confusion_matrix
-    
-    cm = confusion_matrix(y_true, y_pred, normalize='true')
-
-    plt.figure(figsize=(8, 8))
-    sp = plt.subplot(1, 1, 1)
-    ctx = sp.matshow(cm)
-    plt.xticks(list(range(0, 6)), labels=classes)
-    plt.yticks(list(range(0, 6)), labels=classes)
-    plt.colorbar(ctx)
-    plt.show()
-
-    
-#print('Using TensorFlow version', tf.__version__)
-#Custom dataset
-#names = ['id', 'user', 'tweet_id', 'content', 'likes', 'retweets', 'replies']
-#test_dataset2 = pd.read_csv('/Users/achyutpaudel/Desktop/StockPerformanceAnalysis/Tweets/Tweet.csv', on_bad_lines='warn', skipinitialspace=True, names=names)
-#print(test_dataset2.head())
-#trueTest = test_dataset2['content']
-#testList = list()
-#for x in trueTest:
-#    testList.append(x)
-
 
 dataset = nlp.load_dataset('emotion')
 
@@ -66,22 +17,24 @@ def get_tweet(data):
     labels = [x['label'] for x in data]
     return tweets, labels
 
+
+tweets, emLabels = get_tweet(train)
+
+#DEFINE RANDOM GLOBAL VARIABLE FOR TESTS
+randomIndex = random.randint(0, len(tweets))
+
 def print_tweet(t, l):
     # Provided a list of tweets and a list of labels
     # Print a random tweet and its corresponding label
-    randomIndex = random.randint(0, len(t))
     print("tweet:\n", t[randomIndex])
     print("label:", l[randomIndex])
     print()
 
 def print_tokenized(tokenizer, tweets):
     #Print a random tweet and its corresponding tokenized version
-    randomIndex = random.randint(0, len(tweets))
     print("tweet:\n", tweets[randomIndex])
     print("tokenized:", tokenizer.texts_to_sequences(tweets)[randomIndex])
     print()
-
-tweets, emLabels = get_tweet(train)
 
 print_tweet(tweets, emLabels)
 
@@ -121,7 +74,6 @@ def get_sequences(tokenizer, tweets):
 padded_train_seq = get_sequences(tokenizer, tweets)
 
 print("======Padded Test======")
-randomIndex = random.randint(0, len(padded_train_seq))
 print(tweets[randomIndex])
 print(padded_train_seq[randomIndex])
 print()
@@ -163,17 +115,53 @@ val_tweets, val_labels = get_tweet(val)
 val_seq = get_sequences(tokenizer, val_tweets)
 val_labels = names_to_ids(val_labels)
 
-
 #Train the model
 history = model.fit(
     padded_train_seq,
     train_labels,
     validation_data=(val_seq, val_labels),
-    epochs=20,
+    epochs=10,
     callbacks=[
         tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', patience=2)
     ]
 )
+
+
+def show_history(h):
+    epochs_trained = len(h.history['loss'])
+    plt.figure(figsize=(16, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(range(0, epochs_trained), h.history.get('accuracy'), label='Training')
+    plt.plot(range(0, epochs_trained), h.history.get('val_accuracy'), label='Validation')
+    plt.ylim([0., 1.])
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(0, epochs_trained), h.history.get('loss'), label='Training')
+    plt.plot(range(0, epochs_trained), h.history.get('val_loss'), label='Validation')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    
+def show_confusion_matrix(y_true, y_pred, classes):
+    from sklearn.metrics import confusion_matrix
+    
+    cm = confusion_matrix(y_true, y_pred, normalize='true')
+
+    plt.figure(figsize=(8, 8))
+    sp = plt.subplot(1, 1, 1)
+    ctx = sp.matshow(cm)
+    plt.xticks(list(range(0, 6)), labels=classes)
+    plt.yticks(list(range(0, 6)), labels=classes)
+    plt.colorbar(ctx)
+    plt.show()
+
+
 
 #Results
 show_history(history) #Uncomment to see the training history
@@ -184,10 +172,11 @@ test_tweets, test_labels = get_tweet(test_dataset)
 
 #Evaluate the model on the test dataset. 
 #test_tweets is the dataset that is being predicted.
-test_seq = get_sequences(tokenizer, test_dataset)
+test_seq = get_sequences(tokenizer, test_tweets)
 test_labels = names_to_ids(test_labels)
 _ = model.evaluate(test_seq, test_labels)
 
 #Print the confusion matrix to check the accuracy of the model
-preds = model.predict(test_seq)
-show_confusion_matrix(test_labels, preds, index_to_class.values())
+predictions = model.predict(test_seq)
+predictions = np.argmax(predictions, axis=-1)
+show_confusion_matrix(test_labels, predictions, index_to_class.values())
